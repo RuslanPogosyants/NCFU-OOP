693e8d91eee6fd2691ff172124cbccd86ad0ea8d
Ruslan Pogosyants <136241216+RuslanPogosyants@users.noreply.github.com>
2023-10-10 13:59:30 +0300

    Add files via upload

--- RuslanOOP/task_04_03_06/main.py
+++ RuslanOOP/task_04_03_06/main.py
@@ -1,18 +1,63 @@
 # Программирование на языке высокого уровня (Python).
-# Задание №4.3.6 Вариант 2
+# Задание №4.3.6 Вариант 1
 #
 # Выполнил: Погосянц Р.М.
 # Группа: ПИН-б-о-22-1
 # E-mail: ruspogosyants@bk.ru
 
+class Player:
+    def __init__(self):
+        self._is_playing = False
 
-from ПищущаяПринадлежность import ПищущаяПринадлежность
+    def play(self):
+        self._is_playing = True
 
-if __name__ == "__main__":
+    def stop(self):
+        self._is_playing = False
 
-    ПищущаяПринадлежность = ПищущаяПринадлежность()
-    print(ПищущаяПринадлежность)
-    while True:
-        ПищущаяПринадлежность.показать_меню()
-        пункт_меню = input()
-        ПищущаяПринадлежность.обработать_команду(пункт_меню)
+class AudioPlayer(Player):
+    def __init__(self):
+        super().__init__()
+        self._current_track = None
+
+    def play(self, track):
+        super().play()
+        self._current_track = track
+        print(f"Playing audio track '{track}'")
+
+    def stop(self):
+        super().stop()
+        self._current_track = None
+        print("Audio stopped")
+
+class VideoPlayer(Player):
+    def __init__(self):
+        super().__init__()
+        self._current_video = None
+
+    def play(self, video):
+        super().play()
+        self._current_video = video
+        print(f"Playing video '{video}'")
+
+    def stop(self):
+        super().stop()
+        self._current_video = None
+        print("Video stopped")
+
+class DvdPlayer(VideoPlayer):
+    def __init__(self):
+        super().__init__()
+        self._current_position = 0
+
+    def play(self, dvd):
+        super().play(dvd)
+        print(f"Playing DVD '{dvd}'")
+
+    def stop(self):
+        super().stop()
+        self._save_position()
+        print("DVD stopped")
+
+    def _save_position(self):
+        self._current_position = 0
